4d850f344a58b47acb94bb709e5c52bb1b051c65
Jess Belliveau <jess.belliveau@gmail.com>
2013-01-16 09:57:19 +1100

    Further changes. Unique name. Login after signup etc

--- app/models/present.rb
+++ app/models/present.rb
@@ -1,5 +1,7 @@
 class Present < ActiveRecord::Base
-  attr_accessible :content, :user_id, :cost, :link
+  attr_accessible :content, :user_id, :cost, :link, :purchased
+
+	validates :content, presence: true
 
 	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ActiveRecord::Base
 
 	has_many :presents
 
-  validates :name, presence: true, length: { maximum: 50 }
+	VALID_NAME = /\A[a-zA-Z0-9_]{2,30}\Z/
+  validates :name, presence: true, length: { maximum: 50 }, format: { with: VALID_NAME }, uniqueness: { case_sensitive: false }
 	validates :email, presence: true, uniqueness: { case_sensitive: false }
 	validates :password_confirmation, presence: true
 	validates :password, presence: true, length: { minimum: 6 }
 
 	before_save { self.email.downcase! } 
+	before_save { self.name = self.name.slice(0,1).capitalize + self.name.slice(1..-1) }
   before_save :create_remember_token
 
   private
