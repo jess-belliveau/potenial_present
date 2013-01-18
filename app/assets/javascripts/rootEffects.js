cec904bfc1e0e545451040e9c86cf0f2f2d65b91
Jess Belliveau <jess.belliveau@gmail.com>
2013-01-18 15:22:52 +1100

    jQuery CSS improvements

--- app/assets/javascripts/rootEffects.js
+++ app/assets/javascripts/rootEffects.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+	//Basic input change for the Sign in hover
 	$('#nameInput').find('input').attr('value', 'user');
 	$('#nameInput').find('input').click(function(){
 		$(this).attr('value', '');
@@ -10,12 +11,14 @@ $(document).ready(function(){
     $(this).attr('value', '');
   });
 
-
+	//Expand the list capsules on mouseenter
 	$('.present').mouseenter(function(){
 		$(this).find('.details').slideDown();
+		$(this).find('.caret').attr('id', 'pointUp');
 	});
 
 	$('.present').mouseleave(function(){
     $(this).find('.details').slideUp();
+    $(this).find('.caret').attr('id', 'pointDown');
 	});
 });
